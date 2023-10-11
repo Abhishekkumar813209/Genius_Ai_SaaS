46e2fac2e7efc98ad8c29a81562974621d1669e2
Bibash Roy <bibhash@gmail.com>
2023-10-11 23:33:48 +0530

    at 52 min found the logo.png error was in sidebar.tsx because file was not pasted on public folder

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,8 @@
-import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-
 
 export default function DashboardPage() {
   return (
     <div>
         <p> Dashboard Page(Protected) </p>
-        <UserButton afterSignOutUrl = "/" />
     </div>
   )
 }
