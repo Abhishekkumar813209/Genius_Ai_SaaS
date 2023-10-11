b88561ad7717e4f13360038e2b77834e78ae051d
Bibash Roy <bibhash@gmail.com>
2023-10-11 22:33:06 +0530

    at 25min added route groups and more or defined folder structure

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+const LandingPage = () => {
+    return ( 
+        <div>
+            <p>
+              Dashboard Page (Protected)  
+            </p>
+        </div>
+     );
+}
+ 
+export default LandingPage;
